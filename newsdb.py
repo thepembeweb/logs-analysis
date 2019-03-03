abade6bc6a1e6693eaf9992b8214cdd14f547c72
Pemberai Sweto <pembeweb@yahoo.com>
2019-03-03 19:28:46 +0200

    feat: add method to get top three articles

--- newsdb.py
+++ newsdb.py
@@ -10,3 +10,21 @@ def connect(dbname="news"):
         return conn, cursor
     except:
         print("Error in connecting to database")
+
+
+def get_top_three_articles():
+    try:
+        """Return top three articles"""
+        conn, cursor = connect()
+        query = "SELECT articles.title, COUNT(log.path) as views \
+        FROM articles, log \
+        WHERE position(articles.slug in log.path) != 0 \
+        AND log.status = '200 OK' \
+        GROUP BY articles.title \
+        ORDER BY views DESC LIMIT 3;"
+        cursor.execute(query)
+        results = cursor.fetchall()
+        conn.close()
+        return results
+    except:
+        print("Error in returning top three articles")
