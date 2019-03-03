febc10774914ec434accf0c85e0c3c8fe0d5feb5
Pemberai Sweto <pembeweb@yahoo.com>
2019-03-03 19:39:17 +0200

    Revert "feat: add import to display all reports"
    This reverts commit 49e064a0d0666da0bdfdf736d1423a41b25566e5.

--- report.py
+++ report.py
@@ -31,35 +31,3 @@ def show_highest_error_day():
         print(str(info[0]) + " - " + str(round(info[1], 1)) + " % errors.")
     except:
         print("Error occurred showing highest error day")
-
-
-def run_reports():
-    try:
-    """Running Report ..."""
-    print("\n")
-    print("Running reports ...")
-    print("\n")
-
-    print("***************************** Report 1 *****************************")
-    print("\n")
-    show_top_three_articles()
-    print("\n")
-
-    print("***************************** Report 2 *****************************")
-    print("\n")
-    show_popular_authors()
-    print("\n")
-
-    print("***************************** Report 3 *****************************")
-    print("\n")
-    show_highest_error_day()
-    print("\n")
-
-    print("Reports completed!")
-    print("\n")
-    except:
-        print("Error occurred running reports")
-
-
-if __name__ == '__main__':
-    run_reports()
