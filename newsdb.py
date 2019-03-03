246176e520b67b2b95721d4223f345a16c94d1e1
Pemberai Sweto <pembeweb@yahoo.com>
2019-03-03 19:29:47 +0200

    feat: add method to get popular authors

--- newsdb.py
+++ newsdb.py
@@ -28,3 +28,22 @@ def get_top_three_articles():
         return results
     except:
         print("Error in returning top three articles")
+
+
+def get_popular_authors():
+    try:
+        """Return popular authors"""
+        conn, cursor = connect()
+        query = "SELECT authors.name, COUNT(log.path) as view \
+        FROM authors, articles, log \
+        WHERE authors.id = articles.author \
+        AND position(articles.slug in log.path) != 0 \
+        AND log.status = '200 OK' \
+        GROUP BY authors.name \
+        ORDER BY view DESC;"
+        cursor.execute(query)
+        results = cursor.fetchall()
+        conn.close()
+        return results
+    except:
+        print("Error in returning popular authors")
